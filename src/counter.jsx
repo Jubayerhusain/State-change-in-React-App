bfcddc35cac14cb2ab7f42cb545acfd4472768b6
Jubayer husain <jubayerhusai360@gmail.com>
2024-10-18 09:28:17 +0600

    counter done

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 export default function Counter() {
-    return(
-        <div className="counter-box">
-            <h2>Counter: </h2>
-            <button>Add</button>
-        </div>
-    )
+  // distructuring with useState Change [variableName, SetCountFunction] = useState[InitialValue]
+  const [count, setCount] = useState(0);
+  function AddCount() {
+    const newCount = count + 1;
+    return setCount(newCount);
+  }
+  function ReduceCount() {
+    const newCount = count - 1;
+    return setCount(newCount);
+  }
+  return (
+    <div className="counter-box">
+      <h2>Counter: {count}</h2>
+      <button onClick={AddCount}>Add</button>
+      <button onClick={ReduceCount}>Reduce</button>
+    </div>
+  );
 }
